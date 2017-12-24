368128173b244f91cb3587404fb7ba2a35e7d96a
181847 <753090084@qq.com>
2017-12-25 00:10:31 +0800

    在基本的创建窗口的过程外面添加异常处理。

--- AirCraftWar/mainGame.cpp
+++ AirCraftWar/mainGame.cpp
@@ -8,14 +8,26 @@ int main(int argc, char ** argv)
 {
 	HINSTANCE hInstance;
 	hInstance = GetModuleHandle(NULL);
-	EyeEngine::GameWindow mainWindow(hInstance);
 
-	if (!mainWindow.Initialize())
+
+	// Enable run-time memory check for debug builds.
+#if defined(DEBUG) | defined(_DEBUG)
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+	try
 	{
-		std::cout << "mainWindow initialize failed." << std::endl;
+		EyeEngine::GameWindow mainWindow(hInstance);
+		if (!mainWindow.Initialize())
+			return 0;
+
+		return mainWindow.Run();
+	}
+	catch (DxException& e)
+	{
+		MessageBox(nullptr, e.ToString().c_str(), L"HR Failed", MB_OK);
 		return 0;
 	}
-	mainWindow.Run();
 
 	std::cout << "Game end, press enter to quit.." << std::endl;
 	std::getchar();
