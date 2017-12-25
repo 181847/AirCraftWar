4223fe49077c7628838fecf621fdf2d9f069a6d8
181847 <753090084@qq.com>
2017-12-25 19:17:30 +0800

    修改并完善FrameResource.cpp中的构造函数。

--- EyeEngine/FrameResource.cpp
+++ EyeEngine/FrameResource.cpp
@@ -7,21 +7,15 @@ namespace EyeEngine
 
 FrameResource::FrameResource(
 	ID3D12Device * pDevice, 
-	const std::vector<MeshGeometry*>& geos, 
-	const std::vector<UINT>& maxInstancePerGeo, 
 	UINT materialCount)
 {
-	ASSERT(geos.size() != maxInstancePerGeo.size() && "geometry count and instance limitations count didn't match");
-
-	for (UINT geoIndex = 0; geoIndex < geos.size(); ++geoIndex)
-	{
-		_instanceSets.emplace_back(geos[geoIndex], maxInstancePerGeo[geoIndex]);
-	}
-
 	ThrowIfFailed(
 		pDevice->CreateCommandAllocator(
 			D3D12_COMMAND_LIST_TYPE_DIRECT, 
 			IID_PPV_ARGS(_cmdAlloca.GetAddressOf())));
+
+	_passCB = std::make_unique<UploadBuffer<PassConstants>>(pDevice, 1, true);
+	_materialCB = std::make_unique<UploadBuffer<MaterialData>>(pDevice, materialCount, false);
 }
 
 FrameResource::~FrameResource()
