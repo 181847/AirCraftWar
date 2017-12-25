f25dc49b8a45c842802e3448944a4e0aa226e60e
181847 <753090084@qq.com>
2017-12-25 19:22:02 +0800

    实现RenderSystem的部分方法。

--- EyeEngine/RenderSystem.cpp
+++ EyeEngine/RenderSystem.cpp
@@ -1,11 +1,45 @@
 #include "RenderSystem.h"
+#include "D3D12Helper.h"
 
 namespace EyeEngine
 {
-
-RenderSystem::RenderSystem(UINT frameResourceCount)
-	: _frameResources(frameResourceCount)
+RenderSystem::RenderSystem(
+	UINT frameResourceCount, 
+	ID3D12Device*	pDevice,
+	ID3D12CommandQueue * pCmdQueue, 
+	ID3D12CommandList * pCmdList, 
+	ID3D12Fence * pFence, 
+	UINT64 & currFence,
+	const UINT materialCount)
+	:
+	_device(pDevice), 
+	_cmdQueue(pCmdQueue),
+	_cmdList(pCmdList), 
+	_fence(pFence),
+	_currFence(currFence),
+	_numFrameResource(frameResourceCount)
 {
+	for (unsigned int i = 0; i < frameResourceCount; ++i)
+	{
+		_frameResources.push_back(std::make_unique<FrameResource>(pDevice, materialCount));
+	}
+}
+
+RenderSystem::~RenderSystem()
+{
+}
+
+void RenderSystem::TickFrameResource()
+{
+	_currFrameResourceIndex = (_currFrameResourceIndex + 1) % _numFrameResource;
+
+	D3D12Helper::MakeFenceWaitFor(
+		_fence, GetCurrentFrameResouce()->_fenceValue);
+}
+
+FrameResource * RenderSystem::GetCurrentFrameResouce()
+{
+	return _frameResources[_currFrameResourceIndex].get();
 }
 
 }// namespace EyeEngine
