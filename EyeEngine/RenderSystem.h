2eef86f8cab8ff97f1ab2c57e7da226b42b37677
181847 <753090084@qq.com>
2017-12-25 19:21:41 +0800

    为RenderSystem添加成员以及函数。
    开始考虑在RenderSystem中提交绘制数据，首先是准备好每一帧的FrameResource以及相应的同步对象（Fence）。

--- EyeEngine/RenderSystem.h
+++ EyeEngine/RenderSystem.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <DirectX12/Common/d3dUtil.h>
 #include <DirectX12/Common/Camera.h>
+#include <DirectX12/Common/GameTimer.h>
 #include "FrameResource.h"
 
 namespace EyeEngine
@@ -9,24 +10,57 @@ namespace EyeEngine
 class RenderSystem
 {
 protected:
+#pragma region variables about D3D12 Objects
+	ID3D12Device*			_device;
 	ID3D12CommandQueue*		_cmdQueue;
 	ID3D12CommandList*		_cmdList;
+	ID3D12Fence*			_fence;
+#pragma endregion
 
+	// notice:
+	// This is a reference, should be init as the RenderSystem created.
+	UINT64&					_currFence;
+
+#pragma region variables about frameResources
+	const UINT _numFrameResource;
 	std::vector<std::unique_ptr<FrameResource>> _frameResources;
+	// the current frame resource Index
+
+	UINT _currFrameResourceIndex = 0;
+#pragma endregion
 
 public:
-	RenderSystem(UINT frameResourceCount);
+	RenderSystem(
+#pragma region RenderSystem constructor arguments
+		UINT					frameResourceCount,
+		ID3D12Device*			pDevice,
+		ID3D12CommandQueue*		pCmdQueue,
+		ID3D12CommandList*		pCmdList,
+		ID3D12Fence*			pFence,
+		UINT64&					currFence,
+		const UINT				materialCount
+#pragma endregion
+	);
+
 	DELETE_COPY_CONSTRUCTOR(RenderSystem)
 	~RenderSystem();
 
-	template<typename...FRAME_RESOURCE_INIT_ARGS>
-	void Init(FRAME_RESOURCE_INIT_ARGS...args);
-};
+	// update [lights, cameras, new instance, instance pose],
+	// change to another FrameResource to get 
+	void Update(GameTimer& gt);
 
-template<typename ...FRAME_RESOURCE_INIT_ARGS>
-inline void RenderSystem::Init(FRAME_RESOURCE_INIT_ARGS ...args)
-{
-}
+	// Commite drawing command to command queue and execute it.
+	void Draw(GameTimer& gt);
+
+protected:
+	// Switch to the next frameResource, 
+	// and ready to store new data.
+	// Block the thread if the last frameResource is still in use.
+	void TickFrameResource();
+
+	// Get the FrameResource with the index _currFrameResourceIndex.
+	FrameResource* GetCurrentFrameResouce();
+};
 
 }// namespace EyeEngine
 
