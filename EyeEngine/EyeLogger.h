c933cb1a67dbca606cb6b0bf7cefcb6900ea1cee
181847 <753090084@qq.com>
2017-12-26 09:17:32 +0800

    修正全局日志宏中的错误。

--- EyeEngine/EyeLogger.h
+++ EyeEngine/EyeLogger.h
@@ -2,11 +2,11 @@
 #include <log4cplus\logger.h>
 #include <log4cplus\loggingmacros.h>
 
-#define LOG_DEBUG(logMsg)	LOG4CPLUS_DEBUG(g_eyeLogger, errorMsg)
-#define LOG_INFO(logMsg)	LOG4CPLUS_INFO(g_eyeLogger, errorMsg)
-#define LOG_WARN(logMsg)	LOG4CPLUS_WARN(g_eyeLogger, errorMsg)
-#define LOG_DEBUG(logMsg)	LOG4CPLUS_ERROR(g_eyeLogger, errorMsg)
-#define LOG_FATAL(logMsg)	LOG4CPLUS_FATAL(g_eyeLogger, errorMsg)
+#define LOG_DEBUG(logMsg)	LOG4CPLUS_DEBUG(EyeEngine::EyeLogger::g_eyeLogger, logMsg)
+#define LOG_INFO(logMsg)	LOG4CPLUS_INFO(EyeEngine::EyeLogger::g_eyeLogger, logMsg)
+#define LOG_WARN(logMsg)	LOG4CPLUS_WARN(EyeEngine::EyeLogger::g_eyeLogger, logMsg)
+#define LOG_DEBUG(logMsg)	LOG4CPLUS_ERROR(EyeEngine::EyeLogger::g_eyeLogger, logMsg)
+#define LOG_FATAL(logMsg)	LOG4CPLUS_FATAL(EyeEngine::EyeLogger::g_eyeLogger, logMsg)
 
 namespace EyeEngine
 {
