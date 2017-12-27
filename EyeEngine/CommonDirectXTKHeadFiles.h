c9dda24887cf1bb9547cae4f1d4bf3d772ab4e44
181847 <753090084@qq.com>
2017-12-27 21:06:02 +0800

    添加一个方便包含DirectXTK12文件的头文件

---
+++ EyeEngine/CommonDirectXTKHeadFiles.h
@@ -0,0 +1,26 @@
+#pragma once
+
+// Common head files for DirectXTK12
+
+#include "CommonStates.h"
+#include "DDSTextureLoader.h"
+#include "DescriptorHeap.h"
+#include "DirectXHelpers.h"
+#include "EffectPipelineStateDescription.h"
+#include "Effects.h"
+#include "GamePad.h"
+#include "GeometricPrimitive.h"
+#include "GraphicsMemory.h"
+#include "Keyboard.h"
+#include "Model.h"
+#include "Mouse.h"
+#include "PostProcess.h"
+#include "PrimitiveBatch.h"
+#include "RenderTargetState.h"
+#include "ResourceUploadBatch.h"
+#include "ScreenGrab.h"
+#include "SimpleMath.h"
+#include "SpriteBatch.h"
+#include "SpriteFont.h"
+#include "VertexTypes.h"
+#include "WICTextureLoader.h"
