576bd0d2e5a28e740b204ad4f40077db2d4c8e98
181847 <753090084@qq.com>
2017-12-28 15:30:55 +0800

    现在发现log4cplus在多线程下有些问题，现在没有时间过多的考虑这些，所以简化Logger的形式，只在控制台输出。息

--- EyeEngine/EyeLogger.cpp
+++ EyeEngine/EyeLogger.cpp
@@ -1,47 +1,8 @@
 #include "EyeLogger.h"
-#include <log4cplus\configurator.h>
-#include <log4cplus\consoleappender.h>
-#include <log4cplus\layout.h>
 
 #pragma comment(lib, "log4cplusD.lib")
 
 namespace EyeEngine
 {
-namespace EyeLogger
-{
-
-log4cplus::Logger g_eyeLogger;
-
-void InitLogger()
-{
-	using log4cplus::Logger;
-	using log4cplus::helpers::SharedObjectPtr;
-	using log4cplus::Appender;
-	using log4cplus::ConsoleAppender;
-	using log4cplus::PatternLayout;
-
-
-	SharedObjectPtr<Appender> consoleAppender(new ConsoleAppender());
-	consoleAppender->setName(LOG4CPLUS_TEXT("Global ConsoleAppender for EyeEngine"));
-	// Log as this formate:
-	// example start:
-	// 12/25/17 12:59:28 -This is the first log message...[d:\visual studio 2015\projects\learningtests\trylog4cplus\trylog4cplus\trylog4cplus.cpp:42]
-	// example end
-	consoleAppender->setLayout(std::make_unique<PatternLayout>(LOG4CPLUS_TEXT("%d{%m/%d/%y %H:%M:%S} %m[%l]%n")));
-
-	g_eyeLogger = Logger::getInstance(LOG4CPLUS_TEXT("Global Logger for EyeEngine"));
-
-	g_eyeLogger.addAppender(consoleAppender);
-	g_eyeLogger.setLogLevel(log4cplus::ALL_LOG_LEVEL);
-
-	// try to log with all the level
-	LOG4CPLUS_DEBUG(g_eyeLogger, "testing log Debug");
-	LOG4CPLUS_INFO(g_eyeLogger, "testing log Info");
-	LOG4CPLUS_WARN(g_eyeLogger, "testing log Warn");
-	LOG4CPLUS_ERROR(g_eyeLogger, "testing log Error");
-	LOG4CPLUS_FATAL(g_eyeLogger, "testing log Fatal");
-}
-
-}// namespace EyeLogger
 
 }// namespace EyeEngine
--- EyeEngine/EyeLogger.h
+++ EyeEngine/EyeLogger.h
@@ -2,22 +2,17 @@
 #include <log4cplus\logger.h>
 #include <log4cplus\loggingmacros.h>
 
-#define LOG_DEBUG(logMsg)	LOG4CPLUS_DEBUG(EyeEngine::EyeLogger::g_eyeLogger, logMsg)
-#define LOG_INFO(logMsg)	LOG4CPLUS_INFO(EyeEngine::EyeLogger::g_eyeLogger, logMsg)
-#define LOG_WARN(logMsg)	LOG4CPLUS_WARN(EyeEngine::EyeLogger::g_eyeLogger, logMsg)
-#define LOG_ERROR(logMsg)	LOG4CPLUS_ERROR(EyeEngine::EyeLogger::g_eyeLogger, logMsg)
-#define LOG_FATAL(logMsg)	LOG4CPLUS_FATAL(EyeEngine::EyeLogger::g_eyeLogger, logMsg)
-
 namespace EyeEngine
 {
-namespace EyeLogger
+
+class EyeLogger
 {
-
-extern log4cplus::Logger g_eyeLogger;
-
-// Init the global logger for EyeEngine.
-void InitLogger();
-
-}// namespace EyeLogger
+public:
+	template<typename...FORMAT_ARGS>
+	inline static void LogInfo(const std::wstring& fmt, FORMAT_ARGS...args)
+	{
+		std::wprintf(fmt, std::forward<FORMAT_ARGS>(args)...);
+	}
+};
 
 }// namespace EyeEngine
