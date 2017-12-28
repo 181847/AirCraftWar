7131621c933607f38da518b36290da9704683e06
181847 <753090084@qq.com>
2017-12-28 11:10:50 +0800

    在入口函数设置内存分配断点，仍然无法发现是什么地方出错了。

--- AirCraftWar/mainGame.cpp
+++ AirCraftWar/mainGame.cpp
@@ -15,6 +15,7 @@ int main(int argc, char ** argv)
 	// Enable run-time memory check for debug builds.
 #if defined(DEBUG) | defined(_DEBUG)
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	_CrtSetBreakAlloc(166);
 #endif
 
 	try
